d0bf9dee4de6a89af557d0f4b80f2a0d8a054087
Shahed <sk0178397@gmial.com>
2023-12-28 22:37:29 +0600

    Added icon when hover card image.

--- src/components/arowSlider/ArrowSlider.jsx
+++ src/components/arowSlider/ArrowSlider.jsx
@@ -36,9 +36,59 @@ const SliderSection = ({ items, itemsPerSection, currentSlider }) => {
             >
               <div className="relative group">
                 <img className="sm:h-72 " src={item.img} alt={item.title} />
-                <h4 className="absolute group-hover:hidden top-0 left-0 bg-orange-500 h-10 w-10 flex items-center justify-center text-white rounded-full">
+                <h4 className="absolute  group-hover:hidden top-0 left-0 bg-orange-500 h-10 w-10 flex items-center justify-center text-white rounded-full">
                   10%
                 </h4>
+                <div className="bg-gray-400/30 absolute gap-3  hidden top-0 left-0 w-full h-full group-hover:flex duration-300 items-center justify-center">
+                  <span className="h-10 w-10 bg-white rounded-full flex justify-center items-center duration-200 hover:text-orange-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                      />
+                    </svg>
+                  </span>
+                  <span className="h-10 w-10 bg-white rounded-full flex justify-center items-center duration-200 hover:text-orange-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                      />
+                    </svg>
+                  </span>
+                  <span className="h-10 w-10 bg-white rounded-full flex justify-center items-center duration-200 hover:text-orange-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                      />
+                    </svg>
+                  </span>
+                </div>
               </div>
               <div className="py-5 px-3 lg:p-0">
                 <ReactStars
