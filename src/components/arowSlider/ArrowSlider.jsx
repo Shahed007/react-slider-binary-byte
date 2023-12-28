0e485b0ea113d5cba1cb14ced50cd94ed6f77570
Shahed <sk0178397@gmial.com>
2023-12-28 22:08:13 +0600

    Added navigation button.

--- src/components/arowSlider/ArrowSlider.jsx
+++ src/components/arowSlider/ArrowSlider.jsx
@@ -32,12 +32,12 @@ const SliderSection = ({ items, itemsPerSection, currentSlider }) => {
           {section?.map((item, idx) => (
             <div
               key={idx}
-              className={`grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-7 h-full  lg:h-80  `}
+              className={`grid grid-cols-1 lg:grid-cols-2 gap-y-10 py-5 gap-x-8 lg:gap-7 h-full  lg:h-80  `}
             >
               <div>
                 <img className="sm:h-72 " src={item.img} alt={item.title} />
               </div>
-              <div className="py-4 px-3">
+              <div className="py-5 px-3 lg:p-0">
                 <ReactStars
                   count={5}
                   onChange={ratingChanged}
@@ -66,6 +66,25 @@ const SliderSection = ({ items, itemsPerSection, currentSlider }) => {
 const ArrowSlider = () => {
   const { slider1, loading } = useSlider1();
   const [currentSlider, setCurrentSlider] = useState(0);
+  const section = divideItemsIntoSections(slider1, 4);
+  const [prev, setPrev] = useState(false);
+  const [next, setNext] = useState(false);
+
+  const handlePrevious = () => {
+    if (section.length > 0) {
+      setNext(false);
+      setPrev(true);
+      setCurrentSlider(currentSlider - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (section.length - 1 > currentSlider) {
+      setNext(true);
+      setPrev(false);
+      setCurrentSlider(currentSlider + 1);
+    }
+  };
 
   if (loading)
     return (
@@ -81,20 +100,50 @@ const ArrowSlider = () => {
           itemsPerSection={4}
           currentSlider={currentSlider}
         ></SliderSection>
-        <div className="h-[1px] md:block hidden w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-400"></div>
+        <div className="h-[1px] md:block hidden w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-200"></div>
       </div>
 
       <button
-        className="fixed md:absolute top-[47%] left-5"
-        onClick={() => setCurrentSlider(currentSlider - 1)}
+        className={`fixed  md:absolute top-[47%] left-5 z-10 h-10 w-10 rounded-full duration-300 transition-colors bg-white active:scale-95 hover:bg-orange-500  flex justify-center items-center shadow-lg border ${
+          prev ? "bg-orange-500" : ""
+        }`}
+        onClick={handlePrevious}
       >
-        Prev
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5 8.25 12l7.5-7.5"
+          />
+        </svg>
       </button>
       <button
-        className="fixed md:absolute top-[47%] right-5"
-        onClick={() => setCurrentSlider(currentSlider + 1)}
+        className={`fixed md:absolute top-[47%] right-5 z-10 h-10 w-10 rounded-full duration-300 transition-colors bg-white active:scale-95 hover:bg-orange-500  flex justify-center items-center shadow-lg border ${
+          next ? "bg-orange-500" : ""
+        }`}
+        onClick={handleNext}
       >
-        Next
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 "
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m8.25 4.5 7.5 7.5-7.5 7.5"
+          />
+        </svg>
       </button>
     </section>
   );
